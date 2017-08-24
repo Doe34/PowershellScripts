91f3eeed494d6c9e070c6abf610a491a529c5d4c
geen@zaken.mee <geen@zaken.mee>
2017-08-25 01:03:54 +0200

    updates

--- TOOLS_Get-AllPs1.ps1
+++ TOOLS_Get-AllPs1.ps1
@@ -1,16 +1,17 @@
-function Get-AllFunctions
+﻿function Get-AllPs1
 {    #Create an hashtable variable 
     [hashtable]$Return = @{} 
 
 	$Return.scriptName = split-path -leaf $MyInvocation.MyCommand.Definition
 	$Return.rootPath = split-path -parent $MyInvocation.MyCommand.Definition
-	$scripts = gci -re $Return.rootPath -in *.ps*1 | ?{ $_.Name -ne $Return.scriptName }
-	foreach ( $item in $scripts ) {
+	$scripts = gci -re $Return.rootPath -in *.ps1 | ?{ $_.Name -ne $Return.scriptName }
+	<#
+    foreach ( $item in $scripts ) {
 		. $item.FullName
 		#write-host $item.FullName
 	}
 
-	Write-host "All from the found ps*1 files are dot sourced"
-	#return $Return
+    #>
+	return $scripts
 
 }
