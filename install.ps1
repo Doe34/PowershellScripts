f7264498ea94e7f69a7338a40369ee23ddbbe282
geen@zaken.mee <geen@zaken.mee>
2017-09-07 21:22:25 +0200

    updates

---
+++ install.ps1
@@ -0,0 +1,32 @@
+#what is my destination?
+$Destinationpath="c:\PowershellScripts"
+
+if (!(Test-Path $Destinationpath)) {
+    $null = mkdir $Destinationpath
+}
+
+
+#Copy me from github
+$temp = $env:TEMP
+Invoke-WebRequest -Uri "https://github.com/Doe34/PowershellScripts/archive/master.zip" -OutFile "$temp\install.zip"
+
+Expand-ZipFile -Path "$temp\install.zip" -DestinationPath $Destinationpath
+
+robocopy $Destinationpath\PowershellScripts-master $Destinationpath\ /e /mov
+
+Remove-Item $Destinationpath\PowershellScripts-master -recurse
+
+
+#what is the profile name?
+$Profile = "Microsoft.PowerShell_profile.ps1"
+
+
+#Modify profile
+
+
+#Add Destinationpath to the env:path veriable in the profile
+
+
+#Look for functions and add dot source these files in the profile
+
+
