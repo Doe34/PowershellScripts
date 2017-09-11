1fdbc485375419f82f2dff31c3188d127a18f2d3
geen@zaken.mee <geen@zaken.mee>
2017-09-11 23:47:31 +0200

    updates

--- install.ps1
+++ install.ps1
@@ -1,10 +1,8 @@
 # Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-  Exit
- }
+ Write-Host -NoNewLine "Elevated prompt is required - Press any key to continue..."
+ $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+ break 
 }
 ##############################################################################
 
