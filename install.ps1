3c21cbb16ccc266d390e588f0ca7a026e7a247f4
geen@zaken.mee <geen@zaken.mee>
2017-09-11 23:08:21 +0200

    updates

--- install.ps1
+++ install.ps1
@@ -61,7 +61,6 @@ Remove-Item $Destinationpath\PowershellScripts-master -recurse
 #what is the profile name?
 $Profile = "Microsoft.PowerShell_profile.ps1"
 
-$Profile
 #Modify profile
 
   move-item $Destinationpath\$Profile $pshome\$Profile -force -confirm:$false
