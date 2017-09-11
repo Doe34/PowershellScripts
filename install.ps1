574f82a66285e4ed1ed915468e424f0839b9520b
geen@zaken.mee <geen@zaken.mee>
2017-09-11 23:52:44 +0200

    updates

--- install.ps1
+++ install.ps1
@@ -42,8 +42,8 @@ $Profile = "Microsoft.PowerShell_profile.ps1"
   if(-not (get-content $pshome\$Profile | Select-String -Pattern "Destinationpath")){
   Add-Content  $pshome\$Profile $path -force
  
-  #add install.ps1 so it updates itself
-  $install = 'function updateps {iex (New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/Doe34/PowershellScripts/master/install.ps1'')}'
+  #add function for updating powershellscripts
+  $install = 'function updatepss {iex (New-Object Net.WebClient).DownloadString(''https://raw.githubusercontent.com/Doe34/PowershellScripts/master/install.ps1'')}'
   Add-Content  $pshome\$Profile $install -force
   }
   
